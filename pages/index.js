ba0a68013a63e986f98ccf2b40d0a58760d80217
itc-n22009 <n22009@std.it-college.ac.jp>
2023-01-11 06:22:46 +0000

    更新

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home() {
 	return (
-		<Hero 
-		 title="CUBE"
-		 subtitle="アウトプットいくサイト"
-		imageOn
-		/>
+		<Container>
+		  <Hero title="CUBE" subtitle="アウトプットいくサイト" imageOn />
+		</Container>
 	)
 }
