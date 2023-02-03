5dd37e5624d25192a9aa5f5d1ca11188f7f35f98
itc-n22009 <n22009@std.it-college.ac.jp>
2023-02-03 03:10:10 +0000

    chap8

--- components/two-column.js
+++ components/two-column.js
@@ -1,27 +1,25 @@
 import styles from 'styles/two-column.module.css'
 
-export default function TwoColumn({ children }) {
-	return (
-		<div className={styles.flexContainer}>
-		  {children}
-		</div>
-	)
+export default function TwoColumn ({ children }) {
+  return (
+    <div className={styles.flexContainer}>
+      {children}
+    </div>
+  )
 }
 
-
-TwoColumn.Main = function Main({ children }) {
-	return (
-		<div className={styles.main}>
-		  {children}
-		</div>
-	)
+TwoColumn.Main = function Main ({ children }) {
+  return (
+    <div className={styles.main}>
+      {children}
+    </div>
+  )
 }
 
-TwoColumn.Sidebar = function Sidebar({ children }) {
-	return (
-		<div className={styles.sidebar}>
-		  {children}
-		</div>
-	)
+TwoColumn.Sidebar = function Sidebar ({ children }) {
+  return (
+    <div className={styles.sidebar}>
+      {children}
+    </div>
+  )
 }
-
