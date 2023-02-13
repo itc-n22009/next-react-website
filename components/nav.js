30763f074ea43955f00fde7bff9f48acc802892d
itc-n22009 <n22009@std.it-college.ac.jp>
2023-02-13 01:13:14 +0000

    確認

--- components/nav.js
+++ components/nav.js
@@ -1,27 +1,36 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import styles from 'styles/nav.module.css'
 
 export default function Nav () {
-	return (
-		<nav>
-		  <ul className={styles.list}>
-		    <li>
-		        <Link href="/" legacyBehavior>
-			  <a>Home</a>
-		        </Link>
-		    </li>
-		    <li>
-			<Link href="/about" legacyBehavior>
-			  <a>About</a>
-			</Link>
-		    </li>
-		    <li>
-			<Link href="/blog" legacyBehavior>
-			  <a>Blog</a>
-			</Link>
-		    </li>
-		  </ul>
-		</nav>
-	)
-}
+  const [navIsOpen, setNavIsOpen] = useState(false)
 
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
+
+  return (
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>
+        MENU
+      </button>
+      <ul className={styles.list}>
+        <li>
+          <Link href='/' legacyBehavior>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about' legacyBehavior>
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/blog' legacyBehavior>
+            <a>Blog</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
