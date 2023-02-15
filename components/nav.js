d0007dc1dd20a3a29aac68a5c049831fa0482657
itc-n22009 <n22009@std.it-college.ac.jp>
2023-02-15 07:16:50 +0000

    chap10変更

--- components/nav.js
+++ components/nav.js
@@ -9,25 +9,42 @@ export default function Nav () {
     setNavIsOpen((prev) => !prev)
   }
 
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
+
   return (
     <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            } 
+          }
+        `}
+        </style>
+      )}
       <button className={styles.btn} onClick={toggleNav}>
-        MENU
+        <span className={styles.bar} />
+        <span className='sr-only'>MENU</span>
       </button>
       <ul className={styles.list}>
         <li>
           <Link href='/' legacyBehavior>
-            <a>Home</a>
+            <a onClick={closeNav}>Home</a>
           </Link>
         </li>
         <li>
           <Link href='/about' legacyBehavior>
-            <a>About</a>
+            <a onClick={closeNav}>About</a>
           </Link>
         </li>
         <li>
           <Link href='/blog' legacyBehavior>
-            <a>Blog</a>
+            <a onClick={closeNav}>Blog</a>
           </Link>
         </li>
       </ul>
